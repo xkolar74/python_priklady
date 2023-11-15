233c759a0fa427a513b09e4a7de74a4fc956425b
Ondrej Kolar <xkolar74@vutbr.cz>
2023-11-15 16:59:38 +0100

    v1.0

--- grafickakalkulacka.py
+++ grafickakalkulacka.py
@@ -7,27 +7,27 @@ def on_button_click():
 
 # Create the main window
 root = tk.Tk()
-root.title("Simple GUI Application")
+root.title("kalkulačka")
 
 # Create and place the text fields
 text_field1 = tk.Entry(root)
-text_field1.pack(pady=10)
+text_field1.pack(expand=True, fill=tk.BOTH ,pady=10)
 
 text_field2 = tk.Entry(root)
-text_field2.pack(pady=10)
+text_field2.pack(expand=True, fill=tk.BOTH ,pady=10)
 
 # Create and place the buttons
-button1 = tk.Button(root, text="Button 1", command=on_button_click)
-button1.pack(side=tk.LEFT, padx=5)
+button1 = tk.Button(root, text="+", command=on_button_click)
+button1.pack (expand=True, fill=tk.BOTH ,side=tk.LEFT, padx=5)
 
-button2 = tk.Button(root, text="Button 2", command=on_button_click)
-button2.pack(side=tk.LEFT, padx=5)
+button2 = tk.Button(root, text="-", command=on_button_click)
+button2.pack(expand=True, fill=tk.BOTH ,side=tk.LEFT, padx=5)
 
-button3 = tk.Button(root, text="Button 3", command=on_button_click)
-button3.pack(side=tk.LEFT, padx=5)
+button3 = tk.Button(root, text="*", command=on_button_click)
+button3.pack(expand=True, fill=tk.BOTH ,side=tk.LEFT, padx=5)
 
-button4 = tk.Button(root, text="Button 4", command=on_button_click)
-button4.pack(side=tk.LEFT, padx=5)
+button4 = tk.Button(root, text="/", command=on_button_click)
+button4.pack(expand=True, fill=tk.BOTH ,side=tk.LEFT, padx=5)
 
 # Start the Tkinter event loop
 root.mainloop()
